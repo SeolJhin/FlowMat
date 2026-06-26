320d639d4350c0824536da62d6772a02bb7ef0e4
Seoly <rjw0417@gmail.com>
2026-06-26 14:35:03 +0900

    DB & springboot connetion success

--- flowmat_backend/src/main/java/org/myweb/flowmat/Application.java
+++ flowmat_backend/src/main/java/org/myweb/flowmat/Application.java
@@ -2,11 +2,13 @@ package org.myweb.flowmat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableMethodSecurity
 @EnableScheduling
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
